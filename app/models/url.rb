c504a65769665acc7dd3a856dd8e541d86f55e77
Adam Barber <adam@adambarber.tv>
2015-07-25 19:10:18 -0700

    downcase urls and domains before saving them

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ class Url < ActiveRecord::Base
   validates :url, presence: true
   validates :url, uniqueness: { message: 'URL has already been shorted.' }
 
-  before_save :set_domain_name
+  before_save :set_domain_name, :downcase_url
   after_commit :set_slug, on: :create
 
   def shorted_url
@@ -11,8 +11,12 @@ class Url < ActiveRecord::Base
 
   private
 
+  def downcase_url
+    self.url = self.url.downcase
+  end
+
   def set_domain_name
-    self.domain = URI.parse(self.url).host
+    self.domain = URI.parse(self.url).host.downcase
   end
 
   def set_slug
